d5bc20821f6aee7ccf6147abb61340996036198d
ludvins <theludvins@gmail.com>
2018-04-27 15:07:09 +0200

    Corrige

--- practica2/ej20.py
+++ practica2/ej20.py
@@ -1,11 +1,40 @@
-import ejercicio5
+
 import math
+import numpy as num
+import scipy as sci
+from numpy.polynomial import polynomial as pol
+import scipy.integrate as integrate
+import scipy.special as special
+
+
+def rkj(f,a,b,k,j):
+    if(j == 0):
+        h = (b-a)/(2**k)
+        parcial = 0
+        for i in range (2**k - 1):
+            parcial = parcial + f(a+i*h)
+
+        res = (h/2)*(f(a) + 2*parcial +f(b))
+
+    else:
+        res = rkj(f,a,b,k,j-1) + (1/(4**j-1))*(rkj(f,a,b,k,j-1) - rkj(f,a,b,k-1,j-1))
+
+    return res;
+
+
+
+#k = n
+def romberg(f,a,b,n):
+    aprox = rkj(f,a,b,n,n)
+    return aprox
+
+
 
 f1 = lambda x: (x**2)*num.log(x)
 af1 = 1
 bf1 = 1.5
 n1 =1
-f2 = lambda x: (x**3)*.exp(-x)
+f2 = lambda x: (x**3)*math.exp(-x)
 af2 = 0
 bf2 = 1
 n2 = 1
@@ -20,11 +49,15 @@ n4 = 1
 
 
 def calcular(f,a,b,n):
-    while (abs(romberg(f,a,b,n-1) - romberg(f,a,b,n)) < 10**(-6)):
-    frase = "\n" + "Iteracion " + str(n) + ":" + "|R(n-1) - R(n)|" + str(abs(romberg(f,a,b,n-1) - romberg(f,a,b,n)))
-    print(frase)
-    n = n+1
+    it1 = romberg(f,a,b,n-1)
+    it2 = romberg(f,a,b,n)
 
+    while (abs(it1 - it2) > 10**(-6)):
+        it1 = romberg(f,a,b,n-1)
+        it2 = romberg(f,a,b,n)
+        print("\n Iteracion : |R(", n-1,") - R(",n,")|" , abs(it1 - it2) )
+        n = n+1
+'''
 print("Función 1: x^2 * log(x)")
 calcular(f1,af1,bf1,n1)
 v1 = integrate.quad(f1,af1,bf1)
@@ -34,7 +67,7 @@ print("Función 2: x^3 * e^(-x)")
 calcular(f2,af2,bf2,n2)
 v2 = integrate.quad(f2,af2,bf2)
 print("El valor de real era:", v2)
-
+''' 
 print("Función 3: (3x/(x^2 -4))")
 calcular(f3,af3,bf3,n3)
 v3 = integrate.quad(f3,af3,bf3)
