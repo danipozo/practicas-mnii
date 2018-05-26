dadea952a9dc2249067973eeef00a5aff08b50b8
Lauragg <lauragogar@correo.ugr.es>
2018-05-26 13:28:57 +0200

    Arreglada salida y realizada parte de Runge-Kutta del ejercicio 9

--- practica3/Runge-Kutta.py
+++ practica3/Runge-Kutta.py
@@ -7,32 +7,43 @@
 """
 
 
-import numpy as num
-from decimal import *
-import scipy as sci
-from numpy.polynomial import polynomial as pol
+
+import math
+
 
 
 def RungeKutta(f,a,b,n ,y_0):
-    h=Decimal((b-a))/Decimal(n)
+    h=float(b-a)/n
     vals = []
     vals.append(y_0)
-    print("u_0=", vals[0])
+    valor=y(1)
+    print ("Índice\t |  t  |  Aproximado(u) | Real(y) \t\t| Error")
+    print ("0\t |  1  |"+str(y_0)+"\t|"+str(valor)+"  \t| "+str(valor-y_0))
+    """
+        NOTA: Ahora mismo los valores tj se machacan con cada interacción y los
+        valores aproximados u se almacenan en el vector vals. Si se necesita cualquier modificación
+        avisarla y lo cambio
+    """
     for i in range (0, n-1):
-        tj =Decimal(a+(i+1)*h)
+        tj =a+(i+1)*h
         Ki = []
         Ki.append(f(tj,vals[i]))
         Ki.append(f(tj+h/2,vals[i]+(h/2)*Ki[0]))
         Ki.append(f(tj+h/2,vals[i]+(h/2)*Ki[1]))
         Ki.append(f(tj+h,vals[i]+h*Ki[2]))
         x = vals[i] + (h/6)*(Ki[0]+2*Ki[1]+2*Ki[2]+Ki[3])
+        valor=y(tj)
         vals.append(x)
-        print("u_",i+1,"=",x)
+        print (str(i+1)+"\t | "+str(tj)+" |"+str(x)+"\t|"+str(valor)+"  \t| "+str(valor-x))
 
 
 def f(t,x):
-    return -x + t + 1
+    return math.pow(x,2)/(1+t);
 
-f0 = 1
+def y(t):
+    return -1/math.log(t+1)
 
-RungeKutta(f,0,1,10,f0)
+"""En t=1"""
+f0 = -1/(math.log(2))
+
+RungeKutta(f,1,2,10,f0)
