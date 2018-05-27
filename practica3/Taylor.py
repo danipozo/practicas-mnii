cb1fe820f7a9c485551582ec1aba3bd13d7e4af2
Lauragg <lauragogar@correo.ugr.es>
2018-05-27 13:37:03 +0200

    Sustituciones

--- practica3/Taylor.py
+++ practica3/Taylor.py
@@ -15,12 +15,12 @@ def derivadaparcial2 (g, y_0, t_0):
 def genf(r, f, t ,y):
     faux = f
     for i in range(1, r):
-        f = diff(f, y, t) + diff(f, y, t)*faux(t,y)
+        f = diff(f, y, t) + diff(f, y, t)*faux.subs(t,y)
         yield f(y, t)
 
 def T(t,y,h,f,r):
-    print(f(t,y))
-    suma = f(t,y)
+    print(f.subs(t,y))
+    suma = f.subs(t,y)
     for i,f_i in enumerate(genf(r, f, t ,y)):
         suma += (h**i)/(factorial(i))*f_i
 
@@ -44,7 +44,5 @@ b = 1
 n = 10
 y_0 = 1
 
-for i in taylor(g, a, b, n, 1, 3):
+for i in taylor(f, a, b, n, 1, 3):
     print(i)
-
-
