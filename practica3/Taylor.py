654226233939e275b810b79856538ab5c8bd9b31
ludvins <theludvins@gmail.com>
2018-05-27 20:28:38 +0200

    Taylor terminado

--- practica3/Taylor.py
+++ practica3/Taylor.py
@@ -12,19 +12,18 @@ def genf(r, f, t ,y, t_s, y_s):
         yield faux.subs(t_s,t).subs(y_s,y).evalf()
 
 def T(t,y,h,f,r, t_s, y_s):
-    print("T: ",f.subs(t_s,t).subs(y_s,y).evalf())
+
     suma = f.subs(t_s,t).subs(y_s,y).evalf()
     for i,f_i in enumerate(genf(r, f, t ,y, t_s, y_s)):
-        suma += (h**i)/(factorial(i+1))*f_i
+        suma += ((h**(i+1))/(factorial(i+2)))*f_i
 
     return suma
 
 def taylor(f,a,b,n,y_0,r, t_s, y_s):
-
     uj = y_0
     h = (b - a)/n
     tj = a
-    for i in range (1, n+1):
+    for i in range (1, n+2):
         yield uj
         uj = uj + h*T(tj, uj, h, f, r, t_s, y_s)
         tj = tj + h
@@ -39,5 +38,6 @@ b = 1
 n = 10
 y_0 = 1
 
+print(f)
 for i in taylor(f, a, b, n, 1, 2, t, y):
-    print(i)
+    print("Aproximación ", i)
