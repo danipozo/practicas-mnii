f6225583f6ef99d473b414af6d0c5aca64eaa634
Dani Pozo <danipozo@correo.ugr.es>
2018-05-27 18:46:29 +0200

    Evalúa funciones

--- practica3/Taylor.py
+++ practica3/Taylor.py
@@ -2,28 +2,31 @@
 
 import numpy
 import itertools as it
+from math import factorial
 from sympy import symbols, diff
 
-def genf(r, f, t ,y):
+def genf(r, f, t ,y, t_s, y_s):
     faux = f
     for i in range(1, r):
-        f = diff(f, t) + diff(f, y)*faux.subs(t,y)
-        yield f(y, t)
+        faux = diff(faux, t_s) + diff(faux, y_s)*f
+        yield faux.subs(t_s,t).subs(y_s,y).evalf()
 
-def T(t,y,h,f,r):
-    print(f.subs(t,y))
-    suma = f.subs(t,y)
-    for i,f_i in enumerate(genf(r, f, t ,y)):
-        suma += (h**i)/(factorial(i))*f_i
+def T(t,y,h,f,r, t_s, y_s):
+    print("T: ",f.subs(t_s,t).subs(y_s,y).evalf())
+    suma = f.subs(t_s,t).subs(y_s,y).evalf()
+    for i,f_i in enumerate(genf(r, f, t ,y, t_s, y_s)):
+        suma += (h**i)/(factorial(i+1))*f_i
 
-def taylor(f,a,b,n,y_0,r):
+    return suma
+
+def taylor(f,a,b,n,y_0,r, t_s, y_s):
 
     uj = y_0
     h = (b - a)/n
     tj = a
     for i in range (1, n+1):
         yield uj
-        uj = uj + h*T(tj, uj, h, f, r)
+        uj = uj + h*T(tj, uj, h, f, r, t_s, y_s)
         tj = tj + h
 
 
@@ -36,5 +39,5 @@ b = 1
 n = 10
 y_0 = 1
 
-for i in taylor(f, a, b, n, 1, 3):
+for i in taylor(f, a, b, n, 1, 2, t, y):
     print(i)
